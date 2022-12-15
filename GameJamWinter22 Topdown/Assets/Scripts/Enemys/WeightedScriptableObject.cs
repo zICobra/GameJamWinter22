290de497e35707c585dbef2218ede4a772a007bc
Jeremy <100571788+zICobra@users.noreply.github.com>
2022-12-15 23:08:17 +0100

    ínit

---
+++ GameJamWinter22 Topdown/Assets/Scripts/Enemys/WeightedScriptableObject.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+/*[CreateAssetMenu(fileName = "Weighted Spawn Cofing", menuName = "ScriptableObject(weighted Spawn Cofing")]
+public class WeightedScriptableObject : ScriptableObject
+{
+    public EnemyScriptableObject Enemy;
+    [Range(0, 1)] 
+    [SerializeField] float MinWeight;
+    [Range(0, 1)] 
+    [SerializeField] private float MaxWeights;
+    
+    public float GetWeight
+}*/
